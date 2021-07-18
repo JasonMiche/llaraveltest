43a8448ab15957d37d7bf33fdd1700a770d2ebc7
JasonMiche <iasonasm@yahoo.gr>
2021-07-18 11:10:56 +0300

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.layout')
+@extends('resources.css.home')
+@section('content')
+    
+
+<div class="container">
+<h1> Hello world</h1>
+@include('includes.post')
+</div>
+
+
+@endsection
